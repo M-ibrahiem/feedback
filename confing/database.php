853253e52071861513992b04e69cbd8b84899b1f
Mahmoud <mahmoudelsharkawey52@gmail.com>
2024-05-02 15:05:52 +0300

    Native feedback

---
+++ confing/database.php
@@ -0,0 +1,16 @@
+<?php 
+define('DB_HOST','localhost');
+define('DB_USER','feedback');
+define('DB_PASS','feedback');
+define('DB_NAME','feedback');
+
+// Create connection 
+$conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+// check connection
+
+if ($conn->connect_error) {
+    die('Connection Failed '. $conn->connect_error);
+}
+
+// echo 'null';
---
+++ feedback.php
@@ -0,0 +1,65 @@
+<?php
+include('inc/header.php');
+
+
+?>
+<?php
+// $feedback = [
+//   [
+//     'id'    => '1',
+//     'name'  => 'Mahmoud Elsharkawry',
+//     'email' => 'mahmoud@gmail.com',
+//     'body' => ' Hello In mY Feedback',
+//   ],
+//   [
+//     'id'    => '2',
+//     'name'  => 'mohamed Elsharkawry',
+//     'email' => 'mohamed@gmail.com',
+//     'body' => ' Hello In mY Feedback',
+//   ],
+//   [
+//     'id'    => '3',
+//     'name'  => 'doaa Elsharkawry',
+//     'email' => 'doaa@gmail.com',
+//     'body' => ' Hello In mY Feedback',
+//   ],
+// ];
+// $feedback1 = null;
+
+$sql = 'SElECT * FROM feedback';
+$result = mysqli_query($conn,$sql);
+$feedback = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+?>
+
+
+
+<main>
+  <div class="container d-flex flex-column align-items-center">
+   
+    <h2>Past Feedback</h2>
+
+    <?php if(empty($feedback)):?>
+      <p class="lead mt3">There is no feedback</p>
+      <?php endif; ?>
+
+      <?php foreach ($feedback as $item):?>
+          <div class="card my-3 w-75 ">
+            <div class="card-body text-center">
+              <?=$item['body']?>
+            <div class="text-secondary mt-2">
+              By <?=$item['name']?> on
+
+              <?= $item['date']?>
+            </div>
+            </div>
+          </div>
+      <?php endforeach;?>
+
+
+
+  </div>
+</main>
+<?php
+include('inc/footer.php'); 
+?>
